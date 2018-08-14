383c5aed0eee26f6ebab38bbf48c8c0396e82d23
taze-fullstack <taze@fullstack.ph>
2018-08-14 18:35:15 +0800

    consume json trial

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { withSiteData } from 'react-static'
 //
 import logoImg from '../logo.png'
+import cmsHomepage from '../dynamic/homepage.json'
 
 export default withSiteData(() => (
   <div>
     <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
     <img src={logoImg} alt="" />
+    <div>{cmsHomepage.title}</div>
   </div>
 ))
