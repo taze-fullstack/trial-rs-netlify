169c3bbcf6eaf3c2f807ce19d604d9c01fbc8666
taze-fullstack <taze@fullstack.ph>
2018-09-14 14:38:59 +0800

    add widget on homepage

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { withSiteData } from 'react-static'
+import { withSiteData, Head } from 'react-static'
 //
 import logoImg from '../logo.png'
 import cmsHomepage from '../dynamic/homepage.json'
 
 export default withSiteData(() => (
   <div>
+    <Head>
+      <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+    </Head>
     <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
     <img src={logoImg} alt="" />
     <div>{cmsHomepage.title}</div>
